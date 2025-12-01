3e09ae51d7dd648cebec249cb35bb78ce26a2f19
Omer Rahmani <omer@servoya.com>
2025-11-30 23:28:35 -0600

    Fix port back to 8080

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post("/deploy", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Servoya base listening on port ${PORT}`);
